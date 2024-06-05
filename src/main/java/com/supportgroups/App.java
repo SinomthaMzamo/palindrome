8d3ea99bc0f31c51708340207ed4cdcb49c5ee5e
Sinomtha Mzamo <smzamo023@student.wethinkcode.co.za>
2024-06-05 09:44:22 +0200

    initial commit, added src directory and pom.xml to build project

---
+++ src/main/java/com/supportgroups/App.java
@@ -0,0 +1,13 @@
+package com.supportgroups;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
